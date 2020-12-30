e1da0653d55aaee0b3ef71aac9addd568cf9c96d
Robert Siwak <rsiwak@gmail.com>
2020-12-30 21:49:33 +0100

    View ingredients - update sites and classes

---
+++ src/main/java/guru/springframework/controllers/IngredientController.java
@@ -0,0 +1,37 @@
+package guru.springframework.controllers;
+
+import guru.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * <p>Skladniki recepty - controller</p><br>
+ * Company: RS
+ * Project: spring5-recipe-app
+ * Author:  RS
+ * Created: 2020-12-30  17:54
+ */
+@Slf4j
+@Controller
+public class IngredientController {
+
+    private final RecipeService recipeService;
+
+    public IngredientController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping
+    @RequestMapping("/recipe/{recipeId}/ingredients")
+    public String listIngredients(@PathVariable String recipeId, Model model){
+        log.debug("Getting list ingredients, recipe id: " + recipeId);
+        // use command object to avoid lazy load errors in Thymeleaf.
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
+
+        return "recipe/ingredient/list";
+    }
+}
